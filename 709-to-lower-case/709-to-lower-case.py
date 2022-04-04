42c0b0f21b3261fb89a2eaa3dd1a5cb7ff48ae40
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-04 12:17:57 -0400

    Time: 34 ms (75.38%), Space: 13.9 MB (55.19%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -2,7 +2,9 @@ class Solution:
     def toLowerCase(self, s: str) -> str:
         
         arr = list(s)
+        
         for idx,letter in enumerate(arr):
             arr[idx] = letter.lower()
+        
         out = ""
         return out.join(arr)
