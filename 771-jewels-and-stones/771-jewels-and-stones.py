da007e8c0418e8b95134a2e2eacd3618815ae50c
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-05 11:25:46 -0400

    Time: 47 ms (42.88%), Space: 13.9 MB (13.41%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,7 +1,13 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        
+        table = set()
+        for jewel in jewels:
+            table.add(jewel)
+        print(table)
+        
         count = 0
         for stone in stones:
-            if stone in jewels:
+            if stone in table:
                 count += 1
         return count
