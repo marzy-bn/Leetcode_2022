7054b4ece450c40a22d90a4d8ecd4393906660ea
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-07 11:35:45 -0400

    Time: 36 ms (84.05%), Space: 13.9 MB (59.24%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,13 +1,17 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        out = 0
-        p1 = 0
-        while p1 < len(nums)-1:
-            p2 = p1 + 1
-            while p2 < len(nums):
-                if nums[p1] == nums[p2]:
-                    out += 1
-                p2 += 1
-            p1 += 1
-        return out
+        hmap = {}
+        for i,num in enumerate(nums):
+            if num not in hmap:
+                hmap[num] = [i]
+            else:
+                hmap[num].append(i)
+        print(hmap)
+        
+        count = 0
+        for key,val in hmap.items():
+            if len(val) > 1:
+                count += (len(val) * (len(val) - 1) // 2 )
+        return count
+                
             
