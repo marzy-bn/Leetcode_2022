0e5b3c60e0cad273b15ab352202664666ce992de
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-08 13:12:08 -0400

    Time: 364 ms (32.49%), Space: 20.3 MB (5.49%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -4,8 +4,8 @@ class Solution:
         for item in items:
             if ruleValue == item[0] and ruleKey == "type":
                 count += 1
-            elif ruleValue == item[1] and ruleKey == "color":
+            if ruleValue == item[1] and ruleKey == "color":
                 count += 1
-            elif ruleValue == item[2] and ruleKey == "name":
+            if ruleValue == item[2] and ruleKey == "name":
                 count += 1
         return count
