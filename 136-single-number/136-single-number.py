9b28b744b68079a7b6afaa6d61a30e04c6a43426
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-12 10:48:31 -0400

    Time: 152 ms (75.12%), Space: 16.7 MB (53.54%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,24 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hmap = set()
+        #4 1 2 1 2
+        
+        xor = 0
         for num in nums:
-            if num not in hmap:
-                hmap.add(num)
-            else:
-                hmap.remove(num)
+            xor ^= num
+        return xor
         
-        return list(hmap)[-1]
-        
-        
-        '''
-        nums.sort()
-        #1,1,2,2,4
-        i = 0
-        while i < len(nums):
-            if i == len(nums)-1:
-                return nums[i]
-            if nums[i] == nums[i+1]:
-                i += 2
-            else:
-                return nums[i]
-        '''
