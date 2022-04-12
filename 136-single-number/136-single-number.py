bd4765c47cd1800098d0ccdc2fd7ee8af0782589
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-04-12 10:37:48 -0400

    Time: 128 ms (98.30%), Space: 17.2 MB (5.64%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,5 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        hmap = set()
+        for num in nums:
+            if num not in hmap:
+                hmap.add(num)
+            else:
+                hmap.remove(num)
+        
+        return list(hmap)[-1]
+        
+        
+        '''
         nums.sort()
         #1,1,2,2,4
         i = 0
@@ -10,3 +21,4 @@ class Solution:
                 i += 2
             else:
                 return nums[i]
+        '''
