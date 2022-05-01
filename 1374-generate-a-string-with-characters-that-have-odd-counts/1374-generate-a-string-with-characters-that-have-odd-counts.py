36b00c9d74c5c55176b9e7578facd4dbb6486920
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-05-01 13:49:00 -0400

    Time: 41 ms (49.92%), Space: 13.9 MB (51.62%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n == 1:
+            return 'x' * 1
+        if n % 2 == 0:
+            m = n - 1
+            return 'x' * m + 'y' * 1 
+        else:
+            m = n - 2
+            return 'x' * m + 'y' * 1 + 'z' * 1
+        
+        
