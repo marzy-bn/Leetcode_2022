b2fa34e3d5d09a47d353a37930ca41561f381456
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-05-05 17:56:20 -0400

    Time: 564 ms (80.71%), Space: 15.6 MB (93.91%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
         output = ""
-        if len(s) < 2:
+        if len(s) <= 2:
             return s
         output += s[0] + s[1]
         for x,y,z in zip(s,s[1:],s[2:]):
