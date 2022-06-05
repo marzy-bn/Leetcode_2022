c39556640748474cb4d0677b132361cc07847221
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-06-05 12:31:15 -0400

    Time: 175 ms (47.69%), Space: 14.6 MB (49.39%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -5,8 +5,8 @@ class Solution:
         operations = 0
         while i < len(nums)-1:
             if nums[i] >= nums[i+1]:
-                diff = nums[i] - nums[i+1]
-                operations += diff + 1
-                nums[i+1] += diff + 1
+                diff = nums[i] - nums[i+1] + 1
+                operations += diff
+                nums[i+1] += diff
             i += 1
         return operations
